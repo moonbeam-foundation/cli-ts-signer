5a767c31e3f571e8f67e8fccaf5bca655781aeac
Antoine Estienne <estienne.antoine@gmail.com>
2021-05-28 17:45:44 +0000

    implemented sign an d verify for ethereum

--- index.ts
+++ index.ts
@@ -1,8 +1,57 @@
-const argv = require('yargs/yargs')(process.argv.slice(2)).argv;
+import { Keyring } from "@polkadot/keyring";
+import { hexToU8a, numberToHex, u8aToHex, stringToU8a } from "@polkadot/util";
+import type { KeyringPair } from "@polkadot/keyring/types";
+import { verify } from "./verify";
 
-switch(argv._[0]){
-    case "sign":
-        console.log('sign')
-    default:
+const { hideBin } = require("yargs/helpers");
+const argv = require("yargs/yargs")(hideBin(process.argv)).option('signature', {
+    string: true
+}).option('privKey', {
+    string: true
+}).option('pubKey', {
+    string: true
+}).argv;
+function needParam(key:string,functionName:string){
+    if (!argv[key]) {
+      throw new Error(key+" parameter is required for "+functionName);
+    }
+}
+
+let keyring: Keyring;
+switch (argv.type) {
+  case "ethereum":
+    keyring = new Keyring({ type: "ethereum" });
+    break;
+  default:
+    console.log("type defaults to ethereum");
+    keyring = new Keyring({ type: "ethereum" });
+}
+
+switch (argv._[0]) {
+  case "sign":
+    console.log("sign");
+    needParam("message","sign")
+    needParam("privKey","sign")
+    const signer:KeyringPair = keyring.addFromSeed(hexToU8a((argv.privKey)));
+    const signature:Uint8Array = signer.sign(stringToU8a(argv.message));
+    console.log("SIGNATURE : " + u8aToHex(signature));
+    console.log("FOR PUBKEY : " + u8aToHex(signer.publicKey));
+    break;
+    case "verify":
+      console.log("verify");
+      // console.log('signature',argv.signature)
+      needParam("message","verify")
+      needParam("signature","verify")
+      needParam("pubKey","verify")
+    //   const verifier:KeyringPair = keyring.addFromJson(
+    //     JSON.parse('{"address":"KWCv1L3QX9LDPwY4VzvLmarEmXjVJidUzZcinvVnmxAJJCBou","encoded":"U8qFEaghhmNV2PgFhjqzmhyUy37Ok7abfFU2MNsBd0sAgAAAAQAAAAgAAAA3+NniKogzNphiMNueB1X0sGA07B6CaXWfpXPx45iSXoTTprwzU5mOoSqUWO0GKHROI72LN+uJ8Yfv6Ll6JOOV3VPKfoVoFmYm+zDrrMPa0gk5E5kUuSijxADcE6zUrliPVr0Ix/qaghu5SJ7RtWDQLBf4Hp86SJ8Gg6gTSSk=","encoding":{"content":["pkcs8","ethereum"],"type":["scrypt","xsalsa20-poly1305"],"version":"3"},"meta":{}}')
+    //   );
+      // const verifier = keyring.addFromSeed(hexToU8a("0x7dce9bc8babb68fec1409be38c8e1a52650206a7ed90ff956ae8a6d15eeaaef4"))
+     let pubKey=verify((argv.message), ((argv.signature)))
+      console.log("PUBKEY : " + pubKey);
+      console.log(argv.pubKey,pubKey)
+      console.log("VALIDITY : " + ((argv.pubKey)==pubKey).toString());
+      break;
+  default:
     console.log(`function not recognized`);
 }
---
+++ test.ts
@@ -0,0 +1,33 @@
+import { Keyring } from "@polkadot/keyring";
+import { hexToU8a, numberToHex, u8aToHex, stringToU8a } from "@polkadot/util";
+import type { KeyringPair } from "@polkadot/keyring/types";
+ 
+ // combine mnemonic with derivation path
+ const PHRASE = 'seed sock milk update focus rotate barely fade car face mechanic mercy' + '/m/44\'/60\'/0\'/0/0';
+ const PRIV_KEY_ONE = '0x070dc3117300011918e26b02176945cc15c3d548cf49fd8418d97f93af699e46';
+ const ETH_ADDRESS_ONE = '0x31ea8795EE32D782C8ff41a5C68Dcbf0F5B27f6d';
+ const ETH_ADDRESS_TWO = '0x4119b2e6c3Cb618F4f0B93ac77f9BeeC7FF02887';
+
+ let keyring: Keyring;
+
+   keyring = new Keyring({ type: 'ethereum' });
+
+   function expect(statement:boolean){
+       console.log(statement)
+   }
+
+ const MESSAGE = stringToU8a('just some test message');
+const signer = keyring.createFromUri(PHRASE);
+const verifier = keyring.addFromSeed(hexToU8a("0x7dce9bc8babb68fec1409be38c8e1a52650206a7ed90ff956ae8a6d15eeaaef4"))
+// const verifier = keyring.addFromJson(
+//     JSON.parse('{"address":"KWCv1L3QX9LDPwY4VzvLmarEmXjVJidUzZcinvVnmxAJJCBou","encoded":"U8qFEaghhmNV2PgFhjqzmhyUy37Ok7abfFU2MNsBd0sAgAAAAQAAAAgAAAA3+NniKogzNphiMNueB1X0sGA07B6CaXWfpXPx45iSXoTTprwzU5mOoSqUWO0GKHROI72LN+uJ8Yfv6Ll6JOOV3VPKfoVoFmYm+zDrrMPa0gk5E5kUuSijxADcE6zUrliPVr0Ix/qaghu5SJ7RtWDQLBf4Hp86SJ8Gg6gTSSk=","encoding":{"content":["pkcs8","ethereum"],"type":["scrypt","xsalsa20-poly1305"],"version":"3"},"meta":{}}')
+//   );
+
+const signature = signer.sign(MESSAGE);
+const dummyPublic = verifier.publicKey.slice();
+
+dummyPublic[dummyPublic.length - 1] = 0;
+console.log("expecting true false false")
+expect(verifier.verify(MESSAGE, signature, signer.publicKey))
+expect(verifier.verify(MESSAGE, signature, dummyPublic))
+expect(verifier.verify(new Uint8Array(), signature, signer.publicKey))
