1a20339d8e592c76f4bfdb468543b34cc4c659d0
estienne.antoine@gmail.com <estienne.antoine@gmail.com>
2021-07-27 16:12:23 +0100

    modularize gettxdata and submitTx

--- index.ts
+++ index.ts
@@ -1,15 +1,10 @@
 import yargs from "yargs";
 
-import { authorizedChains } from "./methods/utils";
-import { getTransactionData } from "./methods/getTransactionData";
-import { sign } from "./methods/sign";
-import { verify } from "./methods/verify";
-import { createAndSendTx, createAndSendTxPrompt } from "./methods/createAndSendTx";
-import { submitPreSignedTx } from "./methods/submitPreSignedTx";
-import { CreateAndSendArgs, SendTxArgs, SignArgs, VerifyArgs } from "./methods/types";
 import { signCommand, signPromptCommand } from "./commands/signCommand";
 import { createAndSendTxCommand } from "./commands/createAndSendCommand";
 import { verifyCommand } from "./commands/verifyCommand";
+import { getTransactionDataCommand } from "./commands/getTransactionDataCommand";
+import { submitTxCommand } from "./commands/submitTxCommand";
 const { hideBin } = require("yargs/helpers");
 
 export const cli = yargs(hideBin(process.argv))
@@ -17,60 +12,6 @@ export const cli = yargs(hideBin(process.argv))
   .command(signPromptCommand)
   .command(verifyCommand)
   .command(createAndSendTxCommand)
-  .command(
-    "getTransactionData <network> <ws> <address> <tx> <params> [sudo]",
-    "creates a transaction payload and resolves",
-    (yargs) => {
-      yargs
-        .positional("network", {
-          describe: "the network on which you want to send the tx",
-          type: "string",
-          default: "moonbase",
-          choices: authorizedChains,
-        })
-        .positional("ws", {
-          describe: "websocket address of the endpoint on which to connect",
-          type: "string",
-          default: "wss://wss.testnet.moonbeam.network",
-        })
-        .positional("address", {
-          describe: "address of the sender",
-          type: "string",
-        })
-        .positional("tx", {
-          describe: "<pallet>.<function>",
-          type: "string",
-        })
-        .positional("params", {
-          describe: "comma separated list of parameters",
-          type: "string",
-        })
-        .positional("sudo", {
-          describe: "activates sudo mode",
-          type: "boolean",
-        });
-    },
-    (argv: CreateAndSendArgs) => {
-      getTransactionData(argv.tx, argv.params, argv.ws, argv.address, argv.network, argv.sudo);
-    }
-  )
-  .command(
-    "submitTx <ws> <txData>", //TODO: test that with getTransactionData
-    "creates a transaction payload and resolves",
-    (yargs) => {
-      yargs
-        .positional("txData", {
-          describe: "the signed bytecode of the tx you wish to submit on chain",
-          type: "string",
-        })
-        .positional("ws", {
-          describe: "websocket address of the endpoint on which to connect",
-          type: "string",
-          default: "wss://wss.testnet.moonbeam.network",
-        });
-    },
-    (argv: SendTxArgs) => {
-      submitPreSignedTx(argv.ws, argv.txData);
-    }
-  )
+  .command(getTransactionDataCommand) // TODO: test getTransactionDataCommand and submitTxCommand
+  .command(submitTxCommand)
   .help().argv;
--- methods/submitPreSignedTx.ts
+++ methods/submitPreSignedTx.ts
@@ -10,7 +10,7 @@ export async function submitPreSignedTx(ws: string, tx: string): Promise<void> {
 
   // //  eslint-disable-next-line @typescript-eslint/no-floating-promises
   api.rpc.author.submitAndWatchExtrinsic(extrinsic, (result) => {
-    console.log("ok",JSON.stringify(result.toHuman(), null, 2));
+    console.log("ok result",JSON.stringify(result.toHuman(), null, 2));
 
     if (result.isInBlock || result.isFinalized) {
       process.exit(0);
